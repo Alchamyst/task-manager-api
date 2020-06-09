5c8afd176b5e5192e2605f5258573f095f7e78d4
Alchamyst <ashley@ashley-yetman.co.uk>
2020-06-09 18:25:56 +0100

    Add further task tests for sorting and patching

--- tests/task.test.js
+++ tests/task.test.js
@@ -7,6 +7,9 @@ const {
     taskOne, 
     taskTwo,
     taskThree,
+    taskFour,
+    taskFive,
+    taskSix,
     setupDatabase 
 } = require('./fixtures/db')
 
@@ -85,17 +88,55 @@ test ('Should return tasks sorted by description', async () => {
     expect(JSON.stringify(response.body)).toMatch(JSON.stringify(sortedTasks))
 })
 
-// test ('Should return tasks sorted by completed', async () => {
-// })
+ test ('Should return tasks sorted by completed', async () => {
+    const response = await request(app)
+        .get('/tasks?sortBy=completed:desc')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+    const sortedTasks = await Task.find({owner: userOne._id}).sort('-completed')
+    expect(JSON.stringify(response.body)).toMatch(JSON.stringify(sortedTasks))
+    expect(sortedTasks[0]._id).toEqual(taskTwo._id)
+ })
 
-// test ('Should return tasks sorted by createdAt', async () => {
-// })
+test ('Should return tasks sorted by createdAt', async () => {
+    const response = await request(app)
+        .get('/tasks?sortBy=createdAt:desc')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+    const sortedTasks = await Task.find({owner: userOne._id}).sort('-createdAt')
+    expect(JSON.stringify(response.body)).toMatch(JSON.stringify(sortedTasks))
+    expect(sortedTasks[0]._id).toEqual(taskSix._id)
+})
 
-// test ('Should return tasks sorted by updatedAt', async () => {
-// })
+test ('Should return tasks sorted by updatedAt', async () => {
+    const task = await Task.findOne({_id: taskTwo._id})
+    task.description = 'Recently updated task'
+    await task.save()
 
-// test ('Should fetch a page of tasks', async () => {
-// })
+    const response = await request(app)
+        .get('/tasks?sortBy=updatedAt:desc')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+    const sortedTasks = await Task.find({owner: userOne._id}).sort('-updatedAt')
+
+    expect(JSON.stringify(response.body)).toMatch(JSON.stringify(sortedTasks))
+    expect(sortedTasks[0]._id).toEqual(taskTwo._id)
+    expect(sortedTasks[0].description).toEqual('Recently updated task')  
+})
+
+test ('Should fetch a page of tasks', async () => {
+    const response = await request(app)
+        .get('/tasks?limit=2&skip=2')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+    expect(response.body.length).toEqual(2)
+    expect(response.body[0]._id).toMatch(taskFour._id.toString())
+    expect(response.body[1]._id).toMatch(taskFive._id.toString())  
+})
 
 test('Should fetch user task by id', async () => {
     const response = await request(app)
@@ -122,8 +163,16 @@ test('Should not fetch other users task by id', async () => {
     .expect(404)  
 })
 
-// test ('Should update users task by id', async () => {
-// })
+test ('Should update users task by id', async () => {
+    const response = await request(app)
+    .patch(`/tasks/${taskOne._id}`)
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({ "description": "Test my tasks are updating by id" })
+    .expect(200)
+    const task = await Task.findById(taskOne._id)
+    expect(JSON.stringify(task)).toMatch(JSON.stringify(response.body))
+    expect(task.description).toEqual('Test my tasks are updating by id')
+})
 
 test ('Should not update other users tasks', async () => {
     const response = await request(app)
